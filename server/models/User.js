d23e96941f5bc11e4f60c98f00a90bf809bc1c40
patrick-ouano <patrick.ouano123@gmail.com>
2026-03-24 03:55:50 -0400

    Added user schema

---
+++ server/models/User.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: 'Explorer',
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
