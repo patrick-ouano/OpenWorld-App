3ce36b9f8beb4573769e07020241ea2983282708
patrick-ouano <patrick.ouano123@gmail.com>
2026-04-20 04:22:02 -0400

    Added exploredCells on user schema

--- server/models/User.js
+++ server/models/User.js
@@ -18,6 +18,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: 'Explorer',
   },
+  // stores explored map cells per user
+  exploredCells: {
+    type: [String],
+    default: [],
+  },
 });
 
 const User = mongoose.model('User', userSchema);
